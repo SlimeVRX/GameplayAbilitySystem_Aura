f25f6f09d692b19e5e5f2c14670859417037f3c1
slimevrx <slimevrx@gmail.com>
2024-09-30 19:08:54 +0700

    15. Post Process Highlight

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -16,6 +16,7 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 	GENERATED_BODY()
 	
 public: // Functions
+	AAuraEnemy();
 	virtual void HighlightActor() override;
 	virtual void UnhighlightActor() override;
 	
